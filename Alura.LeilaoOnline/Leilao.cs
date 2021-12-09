579244181c1f8a2e6a8040aa9f470406735e5aa8
Kaio Cândido Santiago <62676087+Kaio1394@users.noreply.github.com>
2021-12-09 13:58:59 -0300

    Update Leilao.cs

--- Alura.LeilaoOnline/Leilao.cs
+++ Alura.LeilaoOnline/Leilao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,39 +8,55 @@ namespace Alura.LeilaoOnline
 {
     public class Leilao
     {
+        public enum EstadoLeilao
+        {
+            LeilaoAntesDoPregao,
+            LeilaoEmAndamento,
+            LeilaoFInalizado
+        }
         private IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; }
         public Lance Ganhador { get; private set; }
+        public EstadoLeilao Estado { get; private set; }
+        private Interessada _ultimoCliente; 
 
         public Leilao(string peca)
         {
             Peca = peca;
             _lances = new List<Lance>();
+            Estado = EstadoLeilao.LeilaoAntesDoPregao;
         }
-
+        private bool LanceAceito(Interessada cliente, double valor)
+        {
+            return Estado == EstadoLeilao.LeilaoEmAndamento && cliente != _ultimoCliente;
+        }
         public void RecebeLance(Interessada cliente, double valor)
         {
-            if(valor < 0)
+            if (LanceAceito(cliente, valor))
             {
-                var excessao = new Exception("Valor negativo!!");
-                throw new Exception(excessao.Message);
+                _lances.Add(new Lance(cliente, valor));
+                _ultimoCliente = cliente;
             }
-            _lances.Add(new Lance(cliente, valor));
-
+            
         }
 
         public void IniciaPregao()
         {
-
+            Estado = EstadoLeilao.LeilaoEmAndamento;
         }
 
         public void TerminaPregao()
         {
+            if(Estado != EstadoLeilao.LeilaoEmAndamento)
+            {
+                throw new InvalidOperationException();
+            }
             Ganhador = Lances
                 .DefaultIfEmpty(new Lance(null, 0))
                 .OrderBy(l => l.Valor)
                 .LastOrDefault();
+            Estado = EstadoLeilao.LeilaoFInalizado;
         }
     }
 }
