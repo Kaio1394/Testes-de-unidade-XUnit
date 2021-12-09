6aa08e01143bbebea114e1cf77df00630375705b
Kaio Cândido Santiago <62676087+Kaio1394@users.noreply.github.com>
2021-12-09 10:45:31 -0300

    Add files via upload

---
+++ Alura.LeiaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,36 @@
+﻿using Alura.LeilaoOnline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeiaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Fact]
+        public void NaoPermiteNovosLancesDadoAposLeilaoFinalizado()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fuladno", leilao);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(fulano, 900);
+            
+
+            leilao.TerminaPregao();
+            //Act - método sob teste
+            //Quando o pregão termina.
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            //Então o valor eseprado é o maior valor dado.
+            //      E o cliente ganhador é o que deu o maior lance.
+            var valorEsperado = 2;
+            var valorObtido = leilao.Lances.Count();
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
